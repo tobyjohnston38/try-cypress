cbd51712935b040ee447e678f40732eaf502c416
Toby <toby@crssoftware.co.nz>
2019-06-28 11:57:51 +1200

    working stubs

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -16,9 +16,11 @@ describe("Input form", () => {
   })
 
   context("Form submission", () => {
+    beforeEach(() => {
+      cy.server()
+    })
     it("Adds a new todo on submit", () => {
       const itemText = "Buy eggs"
-      cy.server()
       cy.route("POST", "/api/todos", {
         name: itemText,
         id: 1,
@@ -34,14 +36,19 @@ describe("Input form", () => {
         .should("have.length", 1)
         .and("contain", itemText)
     })
-    it.only("Shows an erorr message on a failed submission", () => {
-      cy.server()
+    it("Shows an erorr message on a failed submission", () => {
       cy.route({
         url: "/api/todos",
         method: "POST",
         status: 500,
         response: {}
       })
+
+      cy.get(".new-todo").type("test{enter}")
+
+      cy.get(".todo-list li").should("not.exist")
+
+      cy.get(".error").should("be.visible")
     })
   })
 })
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -24,12 +24,14 @@ export default class TodoApp extends Component {
   handleTodoSubmit(evt) {
     evt.preventDefault()
     const newTodo = { name: this.state.currentTodo, isComplete: false }
-    saveTodo(newTodo).then(({ data }) =>
-      this.setState({
-        todos: this.state.todos.concat(data),
-        currentTodo: ""
-      })
-    )
+    saveTodo(newTodo)
+      .then(({ data }) =>
+        this.setState({
+          todos: this.state.todos.concat(data),
+          currentTodo: ""
+        })
+      )
+      .catch(() => this.setState({ error: true }))
   }
 
   render() {
@@ -38,6 +40,7 @@ export default class TodoApp extends Component {
         <div>
           <header className="header">
             <h1>todos</h1>
+            {this.state.error ? <span className="error">Oh no!</span> : null}
             <TodoForm
               currentTodo={this.state.currentTodo}
               handleTodoSubmit={this.handleTodoSubmit}
