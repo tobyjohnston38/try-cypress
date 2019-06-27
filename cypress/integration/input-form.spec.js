10b4a70950885b877fb34ecf01c945ebcffb2a3e
Toby <toby@crssoftware.co.nz>
2019-06-28 11:14:19 +1200

    initial commit

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,47 @@
+describe("Input form", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it("focuses input on load", () => {
+    cy.focused().should("have.class", "new-todo")
+  })
+
+  it("accepts input", () => {
+    const typedText = "Buy Milk"
+
+    cy.get(".new-todo")
+      .type(typedText)
+      .should("have.value", typedText)
+  })
+
+  context("Form submission", () => {
+    it("Adds a new todo on submit", () => {
+      const itemText = "Buy eggs"
+      cy.server()
+      cy.route("POST", "/api/todos", {
+        name: itemText,
+        id: 1,
+        isComplete: false
+      })
+
+      cy.get(".new-todo")
+        .type("Buy eggs")
+        .type("{enter}")
+        .should("have.value", "")
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .and("contain", itemText)
+    })
+    it.only("Shows an erorr message on a failed submission", () => {
+      cy.server()
+      cy.route({
+        url: "/api/todos",
+        method: "POST",
+        status: 500,
+        response: {}
+      })
+    })
+  })
+})
